cd0ce420b3653191401df2c07cb5db2348d4c100
Jonathan Oh <jonoh@yelp.com>
2017-09-08 11:40:16 -0700

    added round simulation

--- What To Eat/Controller/FoodChoiceVC.swift
+++ What To Eat/Controller/FoodChoiceVC.swift
@@ -18,37 +18,30 @@ class FoodChoiceVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         currentRound = RoundCreator(lastRoundResults: nil)
         view.backgroundColor = .red
-        guard var currentRound = currentRound else { return }
-        for cuisine in currentRound.eligibleCuisines {
-            print(cuisine.cuisineType.rawValue)
-            print(cuisine.winningTally)
-            //for dish in cuisine.topFiveFoods {
-                //print(dish.dishName.rawValue)
-            //}
-        }
-        print(currentRound.roundNumber)
         
-        for dish in currentRound.choiceOfDishesForUser {
-            print("choice of dishes for user: \(dish.dishName.rawValue)")
-        }
-        
-        roundResults = RoundResults(roundNumber: currentRound.roundNumber, winningDish: currentRound.choiceOfDishesForUser[0], losingDishes: [currentRound.choiceOfDishesForUser[1], currentRound.choiceOfDishesForUser[2]], eligibleCuisines: currentRound.eligibleCuisines)
-        roundResults?.submitDataAndCleanUpRound()
-        currentRound = RoundCreator(lastRoundResults: roundResults)
-        
-        for cuisine in currentRound.eligibleCuisines {
-            //print(cuisine.cuisineType.rawValue)
-            //print(cuisine..winningTally)
-            //print(cuisine.losingTally)
-            for dish in cuisine.topFiveFoods {
-                print("\(dish.dishName.rawValue): Won:\(dish.winningTally), Lost:\(dish.losingTally)")
-            }
-        }
-        print(currentRound.roundNumber)
-        
-        
+        roundSimulation(15)
     }
 
+    func roundSimulation(_ repeatx: Int = 1) {
+        for _ in 0..<repeatx {
+            guard let thisRound = currentRound else { return }
+            print("Round: \(thisRound.roundNumber)")
+            for dish in thisRound.choiceOfDishesForUser {
+                print("choice of dishes for user: \(dish.dishName.rawValue)")
+            }
+            roundResults = RoundResults(roundNumber: thisRound.roundNumber, winningDish: thisRound.choiceOfDishesForUser[0], losingDishes: [thisRound.choiceOfDishesForUser[1], thisRound.choiceOfDishesForUser[2]], eligibleCuisines: thisRound.eligibleCuisines)
+            roundResults!.submitDataAndCleanUpRound()
+            currentRound = RoundCreator(lastRoundResults: roundResults)
+            for cuisine in thisRound.eligibleCuisines {
+                for dish in cuisine.topFiveFoods {
+                    if dish.winningTally > 0 || dish.losingTally > 0 {
+                        print("\(dish.dishName.rawValue): Won:\(dish.winningTally), Lost:\(dish.losingTally)")
+                    }
+                }
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
