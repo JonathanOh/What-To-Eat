9d5636ca5257cc07c1155bed0524a0c6836b3df5
Jonathan Oh <jonathanohs@gmail.com>
2017-09-12 01:44:23 -0700

    added networking files

---
+++ What To Eat/HTTP/HTTPManager.swift
@@ -0,0 +1,11 @@
+//
+//  HTTPManager.swift
+//  What To Eat
+//
+//  Created by admin on 9/12/17.
+//  Copyright © 2017 esohjay. All rights reserved.
+//
+
+import Foundation
+
+// takes an endpoint from a request object and can send and serialize request/responses
---
+++ What To Eat/HTTP/Requests/RestaurantSearchRequest.swift
@@ -0,0 +1,11 @@
+//
+//  RestaurantSearchRequest.swift
+//  What To Eat
+//
+//  Created by admin on 9/12/17.
+//  Copyright © 2017 esohjay. All rights reserved.
+//
+
+import Foundation
+
+// responsible for storing the end point we are requesting to
